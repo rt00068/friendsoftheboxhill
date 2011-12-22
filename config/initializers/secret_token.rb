d4267d774141c0671f5b6fa65698f9ddee2eeebd
Purnima <rt00068@surrey.ac.uk>
2011-12-22 22:43:43 +0000

    Intial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendsofboxhill::Application.config.secret_token = '442bb9aa336b1a8ff7c9be339ed7781e416891f849245b76789d8967f27feabd3d157ba5577ee3cc276df7535477baa840981c804aa8167904b969152765bb44'
